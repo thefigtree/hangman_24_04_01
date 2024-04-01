1cbfae33e3d65829a0303c6b3fb884dea1b295fe
thefigtree <ldj8431@naver.com>
2024-04-01 21:40:18 +0900

    HangmanWord Section

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,31 @@
 export default function HanmanWord() {
-  return;
+  const word = "test";
+  const guessedLetters = ["t", "e", "g"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: "0.1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
